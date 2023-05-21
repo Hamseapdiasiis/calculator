4c63a6e53cb081cb17ae4f665038a58ca3d4f575
Hamseapdiasiis <hamse906@gmail.com>
2023-05-21 17:19:34 +0300

    exercise done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator(num1,num2,operator){
+    if(operator == "+"){
+        return num1 + num2;
+    }else if (operator == "-"){
+        return num1 - num2;
+    }else if(operator == "*"){
+        return num1 * num2;
+    }else if(operator == "/"){
+        return num1 / num2;
+    }else{ return "Invalid Oprator";}
+}
+console.log(calculator (4,2,"+"))
+console.log(calculator (4,2,"-"))
+console.log(calculator (4,2,"*"))
+console.log(calculator (4,2,"/"))
+console.log(calculator (4,2,"%"))
